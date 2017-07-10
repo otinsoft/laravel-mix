783eb8df4ee1fde85ffa0f40914f30633d7b2bbf
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2017-07-10 22:44:42 +0300

    Vue 1 wip

--- src/builder/webpack-rules.js
+++ src/builder/webpack-rules.js
@@ -176,48 +176,48 @@ module.exports = function () {
         test: /\.vue$/,
         loader: 'vue-loader',
         exclude: /bower_components/,
-        options: {
-            // extractCSS: Config.extractVueStyles,
-            loaders: Config.extractVueStyles ? {
-                js: {
-                    loader: 'babel-loader',
-                    options: Config.babel()
-                },
+        // options: {
+        //     // extractCSS: Config.extractVueStyles,
+        //     loaders: Config.extractVueStyles ? {
+        //         js: {
+        //             loader: 'babel-loader',
+        //             options: Config.babel()
+        //         },
 
-                scss: vueExtractPlugin.extract({
-                    use: 'css-loader!sass-loader',
-                    fallback: 'vue-style-loader'
-                }),
+        //         scss: vueExtractPlugin.extract({
+        //             use: 'css-loader!sass-loader',
+        //             fallback: 'vue-style-loader'
+        //         }),
 
-                sass: vueExtractPlugin.extract({
-                    use: 'css-loader!sass-loader?indentedSyntax',
-                    fallback: 'vue-style-loader'
-                }),
+        //         sass: vueExtractPlugin.extract({
+        //             use: 'css-loader!sass-loader?indentedSyntax',
+        //             fallback: 'vue-style-loader'
+        //         }),
 
-                css: vueExtractPlugin.extract({
-                    use: 'css-loader',
-                    fallback: 'vue-style-loader'
-                }),
+        //         css: vueExtractPlugin.extract({
+        //             use: 'css-loader',
+        //             fallback: 'vue-style-loader'
+        //         }),
 
-                stylus: vueExtractPlugin.extract({
-                    use: 'css-loader!stylus-loader?paths[]=node_modules',
-                    fallback: 'vue-style-loader'
-                }),
+        //         stylus: vueExtractPlugin.extract({
+        //             use: 'css-loader!stylus-loader?paths[]=node_modules',
+        //             fallback: 'vue-style-loader'
+        //         }),
 
-                less: vueExtractPlugin.extract({
-                    use: 'css-loader!less-loader',
-                    fallback: 'vue-style-loader'
-                }),
-            } : {
-                js: {
-                    loader: 'babel-loader',
-                    options: Config.babel()
-                }
-            },
-            postcss: Config.postCss,
-            preLoaders: Config.vue.preLoaders,
-            postLoaders: Config.vue.postLoaders
-        }
+        //         less: vueExtractPlugin.extract({
+        //             use: 'css-loader!less-loader',
+        //             fallback: 'vue-style-loader'
+        //         }),
+        //     } : {
+        //         js: {
+        //             loader: 'babel-loader',
+        //             options: Config.babel()
+        //         }
+        //     },
+        //     postcss: Config.postCss,
+        //     preLoaders: Config.vue.preLoaders,
+        //     postLoaders: Config.vue.postLoaders
+        // }
     });
 
     // If there were no existing extract text plugins to add our
